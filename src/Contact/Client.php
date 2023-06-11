8dda70ea768eaac4015f6e26c000ccdaeb3b8447
Bennet Gallein <me@bennetgallein.de>
2023-06-11 14:34:52 +0200

    feat: added filters

--- src/Contact/Client.php
+++ src/Contact/Client.php
@@ -4,22 +4,47 @@ namespace Clicksports\LexOffice\Contact;
 
 use Clicksports\LexOffice\PaginationClient;
 
-class Client extends PaginationClient
-{
+class Client extends PaginationClient {
     protected string $resource = 'contacts';
 
     public string $sortDirection = 'ASC';
 
     public string $sortProperty = 'name';
 
+    private $filters = [];
+
     /**
      * @param int $page
      * @return string
      */
-    public function generateUrl(int $page): string
-    {
+    public function generateUrl(int $page): string {
+        $filters = $this->filters;
+        array_walk($filters, function (&$value, $key) {
+            $value = "{$key}=${value}";
+        });
         return parent::generateUrl($page) .
             '&direction=' . $this->sortDirection .
-            '&property=' .$this->sortProperty;
+            '&property=' . $this->sortProperty . (sizeof($filters) > 0 ? "&" : '') . implode('&', $filters);
+    }
+
+    /**
+     * set filters for the Contacts query. Currently available:
+     * 
+     * - email: filters contacts where any of their email addresses inside the emailAddresses JSON object match the given email value. At least 3 characters are necessary to successfully complete the query.
+     * - name: filters contacts whose name matches the given name value. At least 3 characters are necessary to successfully complete the query.
+     * - number: returns the contacts with the specified contact number. Number is either the customer number or the vendor number located in the roles object.
+     * - customer: if set to true filters contacts that have the role customer. If set to false filters contacts that do not have the customer role.
+     * - vendor: if set to true filters contacts that have the role vendor. If set to false filters contacts that do not have the vendor role.
+     *
+     * @param array $filters
+     * @return void
+     */
+    public function setFilters($filters = []) {
+        $this->filters = $filters;
+    }
+
+    public function resetFilters() {
+        $this->filters = [];
+        return $this;
     }
 }
--- tests/Contact/ClientTest.php
+++ tests/Contact/ClientTest.php
@@ -6,10 +6,8 @@ use Clicksports\LexOffice\Contact\Client;
 use GuzzleHttp\Psr7\Response;
 use Clicksports\LexOffice\Tests\TestClient;
 
-class ClientTest extends TestClient
-{
-    public function testGenerateUrl()
-    {
+class ClientTest extends TestClient {
+    public function testGenerateUrl() {
         $stub = $this->createClientMockObject(
             Client::class,
             new Response(200, [], 'body'),
@@ -22,4 +20,16 @@ class ClientTest extends TestClient
         );
     }
 
+    public function testGenerateUrlWithFilters() {
+        $stub = $this->createClientMockObject(
+            Client::class,
+            new Response(200, [], 'body'),
+            ['generateUrl']
+        );
+
+        $this->assertEquals(
+            'contacts?page=0&size=100&direction=ASC&property=name&name=Bennet',
+            $stub->setFilters(['name' => "Bennet"])->generateUrl(0)
+        );
+    }
 }
